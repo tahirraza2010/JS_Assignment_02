c0c783c55fbd773669b96d740c0ff919f2aad6ff
samiraza19 <samirazagb@gmail.com>
2024-09-11 09:20:19 -0700

    Work Complete

---
+++ app.js
@@ -0,0 +1,385 @@
+// Chapter 6 (Math Expression II)
+// Q1
+// var x = 5; 
+// x++; 
+// console.log(x);
+
+// x = 5;
+// x += 1;
+// console.log(x);
+
+// Q2
+// var x = 100; 
+// --x; 
+// console.log(x);
+
+
+// Q3
+// x = 50;
+// var y = x++;
+// console.log(y);
+
+// Q4
+// x = 50;
+// var y = --x;
+// console.log(y);
+
+// Q5
+// num = 70;
+// var newName = num++;
+// console.log(newName);
+
+// Q6
+// var x = 5;
+// var y = x++; 
+
+// console.log(x); 
+// console.log(y);
+
+// Q7
+//  num = 30;
+// var newName= ++num
+// alert(newName)
+
+// Chapter 7 (Math Expression III)
+// Q1
+// var calculatedNum = 2 + (2 * 6);
+// console.log(calculatedNum)
+// Q2
+// var
+// calculatedNum = (2 + 2) * 6;
+// console.log(calculatedNum)
+// var calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum)
+// Q3
+// var calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum)
+
+// Q4
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// console.log(calculatedNum)
+
+// Q5
+// var cost = (2 + 2) * (4 + 10)
+// console.log(cost)
+
+// Q6
+// var unit = 2 + 2 * 4 + 10;
+// console.log(unit)
+
+// Q7
+// var presure =(4 / (2 * 4))
+// console.log(presure)
+
+
+// Chapter 8 (Concatenating Text Strings)
+// Q1
+// var num = "2" + "2";
+// console.log(num);
+
+// Q2
+// var message = ("Hello" + "Dolly");
+// alert(message)
+
+// Q3
+// alert("33" + 3);
+
+// Q4
+// var q4 = ("Pakistan" + "zindabad");
+// alert(q4)
+
+// Q5
+// var q5 = ("Tahir" + 2010);
+// alert(q5)
+
+// Q6
+// var firtname  = ("mohammad")
+// var lastname =("Tahir")
+// var age =("15")
+// alert(firtname+" " +lastname+ " "+ age)
+
+// Chapter 9 (Prompts)
+// Q1
+// prompt("Enter first name")
+
+// Q2
+// var country= prompt("Country","China")
+// console.log(country)
+
+// Q3
+// var yourName = prompt("Enter Your Name");
+// console.log("You entered: " + yourName);
+
+// Q4
+// var yourName = prompt("Enter Your Message");
+// console.log("You entered: " + yourName);
+
+// Q5
+
+// var message = "What is your name?";
+// var defaultResponse = "Enter your name";
+
+// var userResponse = prompt(message, defaultResponse);
+
+// console.log(userResponse);
+
+// Q6
+// var message = "Message";
+// var defaultResponse = "Enter any message";
+
+// var userResponse = prompt(message, defaultResponse);
+
+// console.log(userResponse);
+
+// alert("================= THE END =================")
+
+// section 2
+// Chapter6 MATH EXPRESSIONS
+// Q1
+// var num1 = 10
+// var num2 = 11
+// var num3 = 12
+
+// document.write("<h1>Result:</h1> <br> The value of a is:"+ num1 + "<br>...........................................<br><br><br> The value of ++a is:"+num2 +"<br> Now the value of a is:"+num2+"<br> <br><br><br>The value of a++ is:"+num2+"<br>Now the value of a is:"+num3+"<br><br><br><br>The value of --a is:"+num2+"<br>Now the value of a is:"+num2+"<br><br><br><br>The value of a-- is:"+num2+"<br>Now the value of  is:"+num1)
+
+// Q2
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// console.log(result)
+
+
+// var result1 = --a
+// console.log(result1)
+
+// var result2 = --a - --b
+// console.log(result2)
+
+// var result3 = --a - --b + ++b
+// console.log(result3)
+
+// var result4 = --a - --b + ++b + b--
+// console.log(result4)
+// document.write("a is 2<br>b is 1<br> result is 3")
+
+// Q3
+// var name = prompt("Please enter your name:")
+//         alert("Hello, " + name + "!");
+//         alert("Hello, Guest!");
+
+// Q5
+// var number = prompt("Enter a number for multiplication Table")
+// document.write("<h1>Multiplication Table of " + number + "</h1>");
+// document.write(number + " x 1 = " + (number * 1) + "<br>");
+// document.write(number + " x 2 = " + (number * 2) + "<br>");
+// document.write(number + " x 3 = " + (number * 3) + "<br>");
+// document.write(number + " x 4 = " + (number * 4) + "<br>");
+// document.write(number + " x 5 = " + (number * 5) + "<br>");
+// document.write(number + " x 6 = " + (number * 6) + "<br>");
+// document.write(number + " x 7 = " + (number * 7) + "<br>");
+// document.write(number + " x 8 = " + (number * 8) + "<br>");
+// document.write(number + " x 9 = " + (number * 9) + "<br>");
+// document.write(number + " x 10 = " + (number * 10) + "<br>");
+
+// Q6
+// a) Take three subjects name from user and store them in 3 different variables.
+// var subject1 = prompt("Enter the name of the first subject:");
+// var subject2 = prompt("Enter the name of the second subject:");
+// var subject3 = prompt("Enter the name of the third subject:");
+
+// // b) Total marks for each subject is 100, store it in another variable.
+// var totalMarksPerSubject = 100;
+
+// // c) Take obtained marks for first subject from user and store it in a different variable.
+// var obtainedMarksSubject1 = parseInt(prompt("Enter obtained marks for " + subject1 + ":"));
+
+// // Output the results to verify
+// document.write("<h3>Subjects and Marks</h3>");
+// document.write("Subject 1: " + subject1 + "<br>");
+// document.write("Subject 2: " + subject2 + "<br>");
+// document.write("Subject 3: " + subject3 + "<br>");
+// document.write("Total marks per subject: " + totalMarksPerSubject + "<br>");
+// document.write("Obtained marks in " + subject1 + ": " + obtainedMarksSubject1 + "<br>");
+
+
+// sectoin 3
+// Chapter 5 MATH EXPRESSIONS
+// Q1
+// var num1 =3
+// var num2 = 5
+// var num3 = 8
+// document.write("Sum of"+" " +num1 +" " +"and"+" " + num2 +" " +"is" +" " +num3)
+
+// Q2
+// var num1 =40
+// var num2 = 20
+// console.log(num1-num2)
+// console.log(num1*num2)
+// console.log(num1/num2)
+// console.log(num1%num2)
+
+// Q3
+
+
+        // // a. Declare a variable.
+        // var myVariable;
+        
+        // // b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
+        // document.write("Value after variable declaration is: " + myVariable + "<br>");
+
+        // // c. Initialize the variable with some number.
+        // myVariable = 5;
+        
+        // // d. Show the value of variable in your browser like “Initial value: 5”.
+        // document.write("Initial value: " + myVariable + "<br>");
+
+        // // e. Increment the variable.
+        // myVariable++;
+        
+        // // f. Show the value of variable in your browser like “Value after increment is: 6”.
+        // document.write("Value after increment is: " + myVariable + "<br>");
+
+        // // g. Add 7 to the variable.
+        // myVariable += 7;
+        
+        // // h. Show the value of variable in your browser like “Value after addition is: 13”.
+        // document.write("Value after addition is: " + myVariable + "<br>");
+
+        // // i. Decrement the variable.
+        // myVariable--;
+        
+        // // j. Show the value of variable in your browser like “Value after decrement is: 12”.
+        // document.write("Value after decrement is: " + myVariable + "<br>");
+
+        // // k. Show the remainder after dividing the variable’s value by 3.
+        // var remainder = myVariable % 3;
+        
+        // // l. Output : “The remainder is : 0”.
+        // document.write("The remainder is: " + remainder);
+        
+
+        // Q4
+//         var price = 500
+// var quantity = 5
+// var total = price * quantity
+// document.write("Total cost to buy " + quantity + " items, each costing " +" "+ total )
+
+// Q5
+
+// var number = 5; 
+
+
+// document.write("<h1>Multiplication Table of " + number + "</h1>");
+// document.write(number + " x 1 = " + (number * 1) + "<br>");
+// document.write(number + " x 2 = " + (number * 2) + "<br>");
+// document.write(number + " x 3 = " + (number * 3) + "<br>");
+// document.write(number + " x 4 = " + (number * 4) + "<br>");
+// document.write(number + " x 5 = " + (number * 5) + "<br>");
+// document.write(number + " x 6 = " + (number * 6) + "<br>");
+// document.write(number + " x 7 = " + (number * 7) + "<br>");
+// document.write(number + " x 8 = " + (number * 8) + "<br>");
+// document.write(number + " x 9 = " + (number * 9) + "<br>");
+// document.write(number + " x 10 = " + (number * 10) + "<br>");
+    
+// Q6
+//  // a. Store a Celsius temperature into a variable.
+//  var celsiusTemp = 30; // Example: 30°C
+        
+//  // b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+//  var fahrenheitFromCelsius = (celsiusTemp * 9/5) + 32;
+//  document.write(celsiusTemp + "°C is " + fahrenheitFromCelsius + "°F.<br>");
+
+//  // c. Now store a Fahrenheit temperature into a variable.
+//  var fahrenheitTemp = 86; // Example: 86°F
+ 
+//  // d. Convert it to Celsius & output “NNoF is NNoC”.
+//  var celsiusFromFahrenheit = (fahrenheitTemp - 32) * 5/9;
+//  document.write(fahrenheitTemp + "°F is " + celsiusFromFahrenheit.toFixed(2) + "°C.");
+
+// Q7
+// var num1 = 1
+// var num2 = 650
+// var num3 = 3
+// var num4 = 2
+// var num5 = 7
+// var num6 = 100
+// var num7 = 2750
+
+// document.write("<h1><b>Shopping Cart</b></h1>")
+// document.write(" Price of item"+" "+ num1+" "+ "is"+" "+ num2)
+// document.write("<br> Quantity of item"+" "+ num1+" "+ "is"+" "+ num3)
+// document.write("<br> Price of item"+" "+ num4+" "+"is"+" "+num6)
+// document.write("<br> Quantity of item"+" "+ num4+" "+"is"+" "+num5)
+// document.write("<br> Shipping Charges"+" "+num6)
+// document.write("<br><br> Total Cost of your order is"+" "+num7)
+    
+// Q8
+// var ms1 = 980
+// var ms2 = 804
+// var ms3 = 82.0408163265306
+// document.write("<h1>Marks Sheet </h1><br>")
+// document.write("Total marks:"+ ms1 +"<br>" )
+// document.write("Marks obtained:"+ ms2 +"<br>")
+// document.write("Percentage:"+ ms3+"%" +"<br>")
+
+// Q9
+// var usAmount = prompt("Enter US Amount")
+// var saAmount = prompt("Enter SA Amount") 
+
+
+// var usToPkr = usAmount * 104.80
+// var saToPkr = saAmount * 28
+
+// var total = usToPkr + saToPkr
+// document.write("<h1>Currency in PKR</h1><br>")
+// document.write("Total Currency in PKR is: " + total)
+
+// Q10
+// var man =5
+// console.log(man+5)
+// console.log(man*10)
+// console.log(man/2)
+
+// Q11
+// var cu=2024
+// var by=2010
+// var age=15
+// document.write("<h1>Age Calculator:</h1><br>")
+// document.write("Current Year:"+cu + "<br>")
+// document.write("Birth Year:"+by+"<br>")
+// document.write("Your Age"+age+" <br>") 
+
+// Q12
+// var radius = 20; 
+        
+// var pi = 3.142;
+// var circumference = 2 * pi * radius;
+
+// var area = pi * radius * radius;
+
+// document.write( "Radius of a circle: " + radius)
+// document.write( "<br>The circumference is " + circumference)
+// document.write( "<br>The area is " + area)
+
+// Q13
+// var favoriteSnack = "Chocolate Chips";
+
+// var currentAge = 25; 
+
+// var maximumAge = 80; 
+
+// var estimatedAmountPerDay = 3;
+
+
+// var totalDays = (maximumAge - currentAge) * 365;
+// var totalSnackNeeded = totalDays * estimatedAmountPerDay;
+
+
+// document.getElementById("result").innerText = "You will need " + totalSnackNeeded + " " + favoriteSnack + " to last you until the ripe old age of " + maximumAge + ".";
+
+// document.write("Favorite Snacks:"+ favoriteSnack)
+
+// document.write("<br/>Current Age:"+ currentAge)
+
+// document.write("<br/>Maximum Age:"+maximumAge)
+
+// document.write("<br/>Amount of snacks per day"+estimatedAmountPerDay)
